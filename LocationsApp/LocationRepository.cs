06b3f406132e7b30f61756c8b349a325f0b6c77a
Local Dev <local+git@localhost>
2021-09-23 15:34:25 -0700

    Add serialization example for location.

---
+++ LocationsApp/LocationRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace LocationsApp
+{
+    // Works whether record or class
+    public record LocationRepository
+    {
+        // Tells client what the JSON field name looks like
+        [JsonPropertyName("name")]
+        public string Name { get; init; }  // the block is required!
+
+        // Error caused by the JSON file changed to use 'position' instead of 'location'
+        [JsonPropertyName("position")] 
+        public List<float> Location { get; init; } // the block is required!
+    }
+}
--- LocationsApp/Program.cs
+++ LocationsApp/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace LocationsApp
 {
     class Program
     {
-        static void Main(string[] args)
+        private static readonly HttpClient client = new HttpClient();
+        private static string DataUrl = "https://raw.githubusercontent.com/chyld/datasets/main/markers.json";
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await ProcessRepositories();
+        }
+        
+        private static async Task ProcessRepositories()
+        {
+            var streamTask = client.GetStreamAsync(DataUrl);
+            var repositories = await JsonSerializer.DeserializeAsync<List<LocationRepository>>(await streamTask);
+            
+            foreach (var repo in repositories)
+                Console.WriteLine(repo.Location[0]);
         }
     }
 }
